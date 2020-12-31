2af4e00b5c13e39bead580804f6a117fa0d23f80
Awaken(Awa) <59164677+A3586@users.noreply.github.com>
2020-12-31 20:02:47 +0700

    Add files via upload

---
+++ Awanote/siteer/account/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.urls import path, include
+import django
+
+
+urlpatterns = [
+    # post views
+    path('',views.dashboard,name='dashboard'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('social-auth/', include('social_django.urls', namespace="social")),
+
+]
