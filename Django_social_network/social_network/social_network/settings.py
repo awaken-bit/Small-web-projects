f09a64fc3ddb484f9223819145cfa9618c3f7069
Awaken(Awa) <59164677+awaken-bit@users.noreply.github.com>
2021-12-02 21:14:31 +0700

    Update settings.py

--- Django_social_network/social_network/social_network/settings.py
+++ Django_social_network/social_network/social_network/settings.py
@@ -185,3 +185,16 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 # -------------------------------
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'account.views.create_new_profiles_gogle'
+)
